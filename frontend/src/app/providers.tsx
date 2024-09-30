7d7db4853523098505a763b9f7b2246517efbda9
Marek Adamczyk <s.marek.adamczyk@gmail.com>
2024-09-30 11:45:24 +0200

    Add Chakra UI

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -3,11 +3,17 @@
 import { PropsWithChildren } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { ApolloWrapper } from '@/wrappers/ApolloWrapper/ApolloWrapper';
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <SessionProvider>
-      <ApolloWrapper>{children}</ApolloWrapper>
+      <ApolloWrapper>
+        <CacheProvider>
+          <ChakraProvider>{children}</ChakraProvider>
+        </CacheProvider>
+      </ApolloWrapper>
     </SessionProvider>
   );
 }
