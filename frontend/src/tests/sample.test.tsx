2084aba24c762919d25ac00627deee8e2907b84d
Marek Adamczyk <s.marek.adamczyk@gmail.com>
2024-11-14 17:02:27 +0100

    Add Jest.js

---
+++ frontend/src/tests/sample.test.tsx
@@ -0,0 +1,10 @@
+// import '@testing-library/jest-dom';
+// import { render, screen } from '@testing-library/react';
+
+// describe('Page', () => {
+//   it('renders a view', () => {
+//     render(<Page />);
+
+//     // expect().toBeInTheDocument();
+//   });
+// });
